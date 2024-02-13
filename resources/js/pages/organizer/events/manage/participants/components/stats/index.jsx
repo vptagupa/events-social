44027b7dd8798ca038578eb9c2cb3533440612af
Vic <victortagupa@gmail.com>
2024-02-13 17:26:32 +0000

    Fix layout

--- resources/js/pages/organizer/events/manage/participants/components/stats/index.jsx
+++ resources/js/pages/organizer/events/manage/participants/components/stats/index.jsx
@@ -1,6 +1,17 @@
 import Stat from "./stat";
 import { stats } from "../../constant";
+import { useState, useEffect } from "react";
 
-export default function Stats() {
-    return stats.map((stat, idx) => <Stat key={idx} value={stat} />);
+export default function Stats({ event }) {
+    const [data, setData] = useState(stats);
+
+    useEffect(() => {
+        axios
+            .post(route("organizer.events.participants.statistics", event.id))
+            .then((res) => {
+                // setData()
+            });
+    }, []);
+
+    return data.map((stat, idx) => <Stat key={idx} value={stat} />);
 }
--- resources/js/pages/organizer/events/manage/participants/sidebar.jsx
+++ resources/js/pages/organizer/events/manage/participants/sidebar.jsx
@@ -3,7 +3,7 @@ import Stats from "./components/stats";
 export default function Sidebar({ event }) {
     return (
         <div className="flex flex-col gap-y-3">
-            <Stats />
+            <Stats event={event} />
         </div>
     );
 }

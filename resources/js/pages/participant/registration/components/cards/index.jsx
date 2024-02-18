158499ee942b1816b50b708053d6c26c963b6ad1
Vic <victortagupa@gmail.com>
2024-02-18 09:37:09 +0000

    Fix grid display

--- resources/js/pages/participant/registration/components/cards/index.jsx
+++ resources/js/pages/participant/registration/components/cards/index.jsx
@@ -5,7 +5,7 @@ import { useContext } from "react";
 export default function Cards({ workshop }) {
     const context = useContext(ControlContext);
     return (
-        <div className="w-full grid md:grid-cols-2 gap-4">
+        <div className="w-full grid md:grid-cols-2 items-start gap-4">
             {context.control.data.map((flex) => (
                 <Card key={flex.flex} value={flex} />
             ))}

098ab13b7a82cff9c6b3bf2bb8057c93d57fdb0d
Vic <victortagupa@gmail.com>
2024-02-20 07:51:22 +0000

    Update routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\Frontend\Auth\ForgotPasswordController;
-use App\Http\Controllers\Admin\Frontend\Auth\ResetPasswordController;
-use App\Http\Controllers\Admin\Frontend\Auth\RegisterController;
-
 /*
 |--------------------------------------------------------------------------
 | Auth Routes
@@ -34,14 +30,15 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
 
     Route::middleware('guest')->group(function () {
-        Route::prefix('forgot-password')->name('forgot-password.')->group(function () {
-            Route::post('/', [ForgotPasswordController::class, 'send'])->name('send');
+        Route::controller(\App\Http\Controllers\Admin\Frontend\Auth\ForgotPasswordController::class)->prefix('forgot-password')->name('forgot-password.')->group(function () {
+            Route::post('/', 'send')->name('send');
         });
-        Route::prefix('reset-password')->name('password.')->group(function () {
-            Route::get('/{token}', [ResetPasswordController::class, 'index'])->name('reset');
-            Route::post('/', [ResetPasswordController::class, 'update'])->name('update');
+
+        Route::controller(\App\Http\Controllers\Admin\Frontend\Auth\ResetPasswordController::class)->prefix('reset-password')->name('password.')->group(function () {
+            Route::get('/{token}', 'index')->name('reset');
+            Route::post('/', 'update')->name('update');
         });
-        Route::post('/register', [RegisterController::class, 'store'])->name('register');
+        Route::post('/register', [\App\Http\Controllers\Admin\Frontend\Auth\RegisterController::class, 'store'])->name('register');
     });
 
 });
@@ -61,7 +58,6 @@ Route::prefix('organizer')->name('organizer.')->group(function () {
             Route::post('/', 'auth')->name('auth');
         })->middleware('guest:organizer');
     });
-
 
     Route::middleware('guest:organizer')->group(function () {
         Route::controller(\App\Http\Controllers\Organizer\Frontend\Auth\ForgotPasswordController::class)->prefix('forgot-password')->name('forgot-password.')->group(function () {

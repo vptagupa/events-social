a3188452464869362d76d6e49bc56093dde412f3
Vic <victortagupa@gmail.com>
2024-02-25 15:34:17 +0000

    Add margin left

--- resources/js/pages/participant/transactions/components/confirm/info.jsx
+++ resources/js/pages/participant/transactions/components/confirm/info.jsx
@@ -3,7 +3,7 @@ import { dateDisplay, currency } from "@/js/helpers";
 
 export default memo(function Info({ value, meta }) {
     return (
-        <div className="w-full md:w-1/2 flex flex-col justify-center items-center text-lg p-3">
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center text-lg p-3 ml-5">
             <div className="w-full flex items-center justify-between border-b-0 border-slate-300">
                 <div className="w-full">
                     Date: {dateDisplay(value.created_at)}

5ac6a98f0f4f5db551fd212d6649435b64ce983f
Vic <victortagupa@gmail.com>
2024-02-27 07:58:55 +0000

    Update tabs style

--- resources/js/pages/organizer/events/manage/certificates/components/tabs.jsx
+++ resources/js/pages/organizer/events/manage/certificates/components/tabs.jsx
@@ -17,17 +17,17 @@ export default function Tabs({ event }) {
     return (
         <div className="w-full px-2 py-2 sm:px-0 relative">
             <Tab.Group>
-                <Tab.List className="flex w-full max-w-md space-x-1 rounded-xl bg-blue-900/20 p-1">
+                <Tab.List className="flex w-full max-w-md">
                     {Object.keys(tabs).map((tab) => (
                         <Tab
                             key={tab}
                             className={({ selected }) =>
                                 classNames(
-                                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5",
-                                    "ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                                    "w-full text-sm font-medium leading-5 py-1.5 transition-all ease-in-out delay-100 duration-500",
+                                    "outline-none focus:outline-none",
                                     selected
-                                        ? "bg-white text-blue-700 shadow"
-                                        : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                                        ? "border-b-[3px] border-slate-500"
+                                        : ""
                                 )
                             }
                         >
@@ -41,7 +41,7 @@ export default function Tabs({ event }) {
                             key={idx}
                             className={classNames(
                                 "rounded-xl bg-white p-3",
-                                "ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                                "ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2 transition-all ease-in-out delay-100 duration-500"
                             )}
                         >
                             {tab}

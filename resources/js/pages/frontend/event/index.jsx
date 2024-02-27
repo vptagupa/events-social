75be5b05fc752422e699d85cf25ee698d6fbc7d0
Vic <victortagupa@gmail.com>
2024-02-27 15:03:11 +0000

    Update event details

--- resources/js/pages/frontend/event/index.jsx
+++ resources/js/pages/frontend/event/index.jsx
@@ -10,11 +10,21 @@ export default function Event({ event }) {
                 Fallback={<p>There was an error while accessing the form</p>}
             >
                 <div className="w-full flex items-center justify-start p-3">
-                    <div className="w-full md:w-1/3 bg-gradient-to-t from-[#BF3131] to-[#BF3131] p-4 text-white rounded-md">
-                        <div className="flex items-center justify-between">
-                            <div className="text-2xl">{event.title}</div>
-                            <div className="text-xs">
-                                {dateDisplay(event.expected_start_at)}
+                    <div className="w-full md:w-1/3 flex flex-col items-center justify-start gap-y-2 bg-gradient-to-t from-[#BF3131] to-[#BF3131] p-4 text-white rounded-md">
+                        <div className="w-full flex items-center justify-between">
+                            <div className="text-2xl w-full">{event.title}</div>
+                        </div>
+                        <div className="w-full flex flex-col items-center justify-start">
+                            <div className="text-lg w-full">
+                                {" "}
+                                {dateDisplay(
+                                    event.expected_start_at,
+                                    "MMMM DD, YYYY"
+                                )}
+                            </div>
+                            <div className="text-lg w-full">{event.place}</div>
+                            <div className="text-sm w-full">
+                                {event.address}
                             </div>
                         </div>
                     </div>

cae9ea2ad3a79105c9aa0969a579c1d08c865ae8
Vic <victortagupa@gmail.com>
2024-03-10 04:09:13 +0000

    Update markdown

--- app/Notifications/Participant/Invitation.php
+++ app/Notifications/Participant/Invitation.php
@@ -7,19 +7,18 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use chillerlan\QRCode\QRCode;
 
 class Invitation extends Notification
 {
     use Queueable;
-
-    public $event;
 
     /**
      * Create a new notification instance.
      */
     public function __construct(public Workshop $workshop)
     {
-        $this->event = $this->workshop->event;
+
     }
 
     /**
@@ -40,17 +39,12 @@ class Invitation extends Notification
         $name = $this->workshop->name;
 
         return(new MailMessage)
-            ->subject('Invitation to Join ' . $this->event->title)
-            ->greeting(empty($name) ? 'Hello' : 'Dear ' . $name)
-            ->line('We are excited to invite you to join us for ' . $this->workshop->event->title . '! As a valued member of our community, we would be delighted to have you participate in this event.')
-            ->line('Event Details:')
-            ->line('Date: ' . $this->workshop->event->expected_start_at->format('m/d/Y'))
-            ->line('Time: ' . $this->event->expected_start_at->format('h:i a'))
-            ->line('Place: ' . $this->workshop->event->place)
-            ->line('Address: ' . $this->workshop->event->address)
-            ->action('Accept Invite', url(route('invitation.accepted', $this->workshop->uuid)))
-            ->line("If you have any questions or need further information, feel free to contact us.")
-            ->line('We look forward to seeing you at the event!');
+            ->subject('Invitation to Join ' . $this->workshop->event->title)
+            ->markdown('mail.invitation', [
+                'workshop' => $this->workshop,
+                'qrcode' => (new QRCode)->render($this->workshop->uuid)
+            ]);
+
     }
 
     /**

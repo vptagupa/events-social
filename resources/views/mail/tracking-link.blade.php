3abb330a64b1c0f2ce105a94fa5c32d36bc0b48a
Vic <victortagupa@gmail.com>
2024-03-10 14:38:16 +0000

    Use iframe

--- resources/views/mail/tracking-link.blade.php
+++ resources/views/mail/tracking-link.blade.php
@@ -14,7 +14,7 @@ Continue
 
 <p>The organizer will scan the QR code below for verification and attendance.</p>
 <div style="width: 100%; text-align:center;">
-    <img src="{{route('qrcode', $workshop->uuid)}}" alt="{{$workshop->uuid}}" style="width: 200px;text-align:center;" />
+<iframe src="{{route('qrcode', $workshop->uuid)}}" title="{{$workshop->uuid}}"></iframe>
 </div>
 
 <p>See you!</p>
